c1b6f91d85c7ce6b6ea4ba3c92c13137f3f23a2d
kento-96 <kento.soccer.1107@gmail.com>
2021-05-09 12:25:45 +0000

    [create]book,user_view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,18 +7,50 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book=Book.new(book_params)
+    @book.user_id=current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render:index
+    end
+    @user=User.find(current_user.id)
+    @books=Book.all
   end
 
   def show
+     @book=Book.find(params[:id])
+    @user=User.find(@book.user_id)
+    @new=Book.new
+    @books=Book.all
   end
 
   def edit
+    @book=Book.find(params[:id])
+    if @book.user == current_user
+      render"edit"
+    else
+      redirect_to books_path
+    end
   end
 
   def update
+    @book=Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
+ private
+ def book_params
+   params.require(:book).permit(:title,:body)
+ end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6dae1b675e34dc594d4bea1aae7015e589e42b7da851a4a60ff6c0d67f2190c8ac9f6d63ffd563cd2b7afd38a544ace971ef94e5149e55c714272290294f79d0'
