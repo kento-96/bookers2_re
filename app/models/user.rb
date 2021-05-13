94363fd92d9c8ff68c1a84f2c00d83b0f2c32ce2
kento-96 <kento.soccer.1107@gmail.com>
2021-05-13 12:30:50 +0000

    検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,20 @@ class User < ApplicationRecord
     followings.include?(user)
   end
   
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+   end
+  
   validates :name, uniqueness:true , presence:true, length: {maximum:20, minimum:2}
   validates :introduction,  length:{maximum:50}
 
